889d7e441963785e84a06708ad45328f8a618301
vortex73 <nsreekumar6@gmail.com>
2024-03-27 01:14:04 +0530

    final

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 obj-m += process_tree.o
+ccflags-remove-y += -mrecord-mcount -fconserve-stack
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
--- process_tree.c
+++ process_tree.c
@@ -5,20 +5,23 @@
 #include <linux/init.h>
 #include <linux/sched/signal.h>
 
-static int __init process_tree_init(void) {
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Narayan");
+MODULE_DESCRIPTION("A seksi module made for absolutely nothing");
+
+static int __init proc_tree(void) {
     struct task_struct *task;
     struct task_struct *child;
     struct list_head *list;
 
-    printk(KERN_INFO "Initializing Process Tree Module\n");
+    printk(KERN_INFO "Beginning Tree Listing...\n");
 
     for_each_process(task) {
         printk(KERN_INFO "Parent Process: %s [%d]\n", task->comm, task->pid);
 
         list_for_each(list, &task->children) {
             child = list_entry(list, struct task_struct, sibling);
-            printk(KERN_INFO " |");
-            printk(KERN_INFO " - Child Process: %s [%d]\n", child->comm, child->pid);
+            printk(KERN_INFO " |- Child Process: %s [%d]\n", child->comm, child->pid);
         }
     }
 
@@ -26,10 +29,9 @@ static int __init process_tree_init(void) {
 }
 
 static void __exit process_tree_exit(void) {
-    printk(KERN_INFO "Exiting Process Tree Module\n");
+    printk(KERN_INFO "Cleaning up...\n");
 }
 
-module_init(process_tree_init);
+module_init(proc_tree);
 module_exit(process_tree_exit);
-
 
